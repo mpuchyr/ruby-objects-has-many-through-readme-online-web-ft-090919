f1ce855fa63b43ceff48c445b48afa383f2ea1dd
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-19 21:35:43 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -15,17 +15,5 @@ class Meal
     @@all
   end
   
-  def meals
-    Meal.all.select do |meal|
-      meal.waiter == self
-    end
-  end
-  
-  def best_tipper
-    best_tipped_meal = meals.max do |meal_a, meal_b|
-      meal_a.tip <=> meal_b.tip
-    end
-    
-    best_tipped_meal.customer
-  end
+
 end
